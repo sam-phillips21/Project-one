ca4841c97375931cacf7cc411d2b371d8830bd8a
sam-phillips21 <phillips.sam@gmail.com>
2022-09-26 07:14:10 -0600

    older working version

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//function moves player
+// console.log('js is linked and working')
+//function moves player and allows mountianeer
 //function to generate cracks in play board
 //function for collision detection of player and crack in ice
-// function to loop play board
-//timer function= time played = points earned
+//function to remove a life if player falls in ice
 //step one: move player
 //step two: generate cracks
 //step three: loop cracks
+// space bar starts game
+
 const canvas = document.getElementById('canvas');
 const movement = document.getElementById('movement');
 const status = document.getElementById('status');
 const ctx = canvas.getContext('2d');
-const button = document.getElementById('btn')
-let contact = true
 
-const coordinateArray = [
+const ice = [
     {
         x: 40,
         y: 0,
@@ -92,32 +92,32 @@ const coordinateArray = [
         width: 10,
         height: 75
     },
-
+    {
+        x: 40,
+        y: 0,
+        width: 10,
+        height: 75,
+    },
+    {
+        x: 80,
+        y: 0,
+        width: 10,
+        height: 60,
+    }, 
+     {
+        x: 110,
+        y: 0,
+        width: 10,
+        height: 75,
+    },
+    {
+        x: 160,
+        y: 0,
+        width: 10,
+        height: 60,
+    },
     
  ]
- class IceField {
-    constructor() {
-        this.iceField = coordinateArray
-    }
-    addIce(x, y, width, height) {
-        this.iceField.forEach((i) => {
-        this.iceField.push(i)
-    })
-    console.log(this.iceField)
- }
- drawIce() {
-    this.iceField.forEach((i) => {
-    ctx.fillStyle = 'lightblue'
-    ctx.fillRect(i.x, i.y , i.width, i.height)
-})
- }
-moveLeft() {
-    this.iceField.forEach((i) => {
-        (i.x = i.x-1)
-     })
-    }
- }
-let iceField = new IceField()
 class Mtneer {
     constructor(x, y, color, width, height, alive) {
         this.x = x,
@@ -132,15 +132,16 @@ class Mtneer {
             down: false,
             left: false,
             right: false,
-        }
+        },
         this.setDirection = function (key) {
+
             if (['w', 'a', 's', 'd']) {
                 if (event.key == 'w') {this.direction.up = true}       
             }
-        }
+        }, 
         this.drawMtneer = () => {
             ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height)
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
 }
@@ -152,47 +153,58 @@ const listenForKeysFunction = (character) => {
         if (keypress == 'w') { character.y -= 3 }
         if (keypress == 'a') { character.x -= 3 }
         if (keypress == 's') { character.y += 3 }
-        if (keypress == 'd') { character.x += 3 }   
+        if (keypress == 'd') { character.x += 3 }  
   })
 }
 listenForKeysFunction(mtneerSam)
-
+const drawIce = (ice) => {
+   for (let i = 0; i < ice.length; i++) {
+    ctx.fillStyle = 'lightblue';
+    ctx.fillRect(ice[i].x, ice[i].y, ice[i].width, ice[i].height)
+   }
+    
+}
+const moveLeft = (ice) => {
+    ice.forEach((i) => {
+        i.x = i.x - 1
+    })  
+}
 const collisionDetection = () => {
-    iceField.iceField.forEach((i) => {
-        while( mtneerSam.x < i.width + i.height
+   
+    ice.forEach((i) => {
+        if( mtneerSam.x < i.width + i.height
             && mtneerSam.x + mtneerSam.width > i.x
             && mtneerSam.y < i.y + i.height
-            && mtneerSam.y + mtneerSam.height > i.y 
-            && mtneerSam.alive ==false 
-            && !contact == true)
-            clearInterval(gameLoop)  
-          
+            && mtneerSam.y + mtneerSam.height > i.y) {
+                mtneerSam.alive = false 
+                pauseGameLoop()
+            }
+    
     })
+    
 }
-
 const gameLoop = () => {
+    // console.log('this is running')
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // drawMtneer(mtneer);
+    drawIce(ice)
+    moveLeft(ice)
     mtneerSam.drawMtneer()
-    iceField.drawIce()
-    iceField.moveLeft()
     collisionDetection()
-    pauseGameLoop()
-   
+
+}
+const pauseGameLoop = () => {
+    if (mtneerSam.alive == false)
+    setTimeout(gameLoop)
+    ctx.font = "30px Arial";
+    ctx.strokeText("you have fallen in the crack!", 10, 50);
 }
 
-const pauseGameLoop = () => {
-    if (mtneerSam.alive == false) {
-        ctx.font = "10px Arial",
-        ctx.strokeText("You have fallen to you death", 10, 50)
-        setTimeout(gameLoop)
-    } 
-    // clearInterval(gameLoop)  
-}
 setInterval(gameLoop,500) //16.6 60fps, 
 
+
 let reset = ((pauseGameLoop) => {
-resetButton()
-})
-document.addEventListener('DOMContentLoaded', resetButton)
-resetBtn.addEventListener('click', reset)
-         
+    resetButton()
+    })
+    document.addEventListener('DOMContentLoaded', resetButton)
+    resetBtn.addEventListener('click', reset)
